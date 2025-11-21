dcb337f69e457716120743a5e14f4183d35584c0
Galih Lazuardi <galihlazuardi@upi.edu>
2025-11-21 20:03:56 +0700

    load more data

--- src/components/load-more-data/LoadMoreData.jsx
+++ src/components/load-more-data/LoadMoreData.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import style from "./loadmoredata.module.css";
 
 const LoadMoreData = () => {
-  return (
-    <div>LoadMoreData</div>
-  )
-}
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [disableButton, setDisableButton] = useState(false);
 
-export default LoadMoreData
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        setLoading(true);
+
+        const response = await fetch(
+          `https://dummyjson.com/products?limit=20&skip=${
+            count === 0 ? 0 : count * 20
+          }`
+        );
+        if (!response.ok) throw new Error(`HTTP Error: ${response.status}`);
+
+        const data = await response.json();
+        if (data && data.products && data.products.length) {
+          // data?.products?.length nulisnya bisa gini juga buat kondisinya
+          setProducts((prevProducts) => [...prevProducts, ...data.products]);
+        }
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+        setProducts([]);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchProducts();
+  }, [count]);
+
+  useEffect(() => {
+    if (products.length >= 100) {
+      setDisableButton(true);
+    }
+  }, [products]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className={style["load-more-container"]}>
+      <div className={style["product-container"]}>
+        {products && products.length ? (
+          products.map((product) => (
+            <div key={product.id} className={style.product}>
+              <img src={product.images[0]} alt={product.title} />
+              <h3>{product.title}</h3>
+              <p>{product.description}</p>
+              <p>Price: ${product.price}</p>
+            </div>
+          ))
+        ) : (
+          <p>No products available.</p>
+        )}
+      </div>
+      <div className={style["button-container"]}>
+        <button disabled={disableButton} onClick={() => setCount(count + 1)}>Load More Products</button>
+      </div>
+      {
+        disableButton && <p>You have reached the maximum number of products.</p>
+      }
+    </div>
+  );
+};
+
+export default LoadMoreData;
